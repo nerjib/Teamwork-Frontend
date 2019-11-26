330399e835563865aac9071a0d35afa5b6dec5ca
kabirnjib0@gmail.com <kabirnajib0@gmail.com>
2019-11-26 23:43:27 +0100

    Updates

---
+++ precache-manifest.ae12f3ce5aa196dc31cb660961a7b1bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5c280182630f244906e3a2b34aa61e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c65e282a07a5653a0f7",
+    "url": "/static/css/main.4b33a866.chunk.css"
+  },
+  {
+    "revision": "0b7c8514bd4abc05a9a7",
+    "url": "/static/js/2.9e7c3291.chunk.js"
+  },
+  {
+    "revision": "1c65e282a07a5653a0f7",
+    "url": "/static/js/main.8c1693d9.chunk.js"
+  },
+  {
+    "revision": "cd1afeb3ed6272ff8ebd",
+    "url": "/static/js/runtime-main.bbff87a9.js"
+  }
+]);
