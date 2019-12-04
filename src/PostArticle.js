d2b4fcda01f8d48637eaad173988c82971e781aa
kabirnjib0@gmail.com <kabirnajib0@gmail.com>
2019-12-04 16:12:04 +0100

    upload image componet added

--- src/PostArticle.js
+++ src/PostArticle.js
@@ -5,8 +5,12 @@ export default class PostArticle extends Component {
     super(props)
     this.state = {
      title : '',
+     gifTitle: '',
       article: '',
-      status: ''
+      status: '',
+      image:null,
+      content:'',
+
     };
   }
 
@@ -16,6 +20,39 @@ export default class PostArticle extends Component {
       [name]: value
     });
   }
+
+handleImageChange = (e) => {
+  this.setState({
+    image: e.target.files[0]
+  })
+};
+handleSubmit = (e) => {
+  e.preventDefault();
+  console.log(this.state);
+  let form_data = new FormData();
+  form_data.append('image', this.state.image);
+  form_data.append('title', this.state.gifTitle);
+  fetch('https://powerful-garden-82332.herokuapp.com/api/v1/gifs',{
+      method: 'POST',
+      body: form_data,
+      headers: {
+        token: localStorage.getItem('token')
+      }
+    }).then(res => {
+      if (res.status === 201) {
+        this.setState({
+            status: 'Success'
+        });
+      } else {
+        const error = new Error(res.error);
+        throw error;
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      alert('Error image not uploaded');
+    });
+};
 
   onSubmit = (event) => {
     event.preventDefault();
@@ -39,7 +76,7 @@ export default class PostArticle extends Component {
     })
     .catch(err => {
       console.error(err);
-      alert('Posting Error');
+      alert('posting error');
     });
   }
 
@@ -64,17 +101,35 @@ export default class PostArticle extends Component {
           onChange={this.handleInputChange}
           required
         />
-                {this.state.status}
-      <input
-          type="file"
-          name="image"
-          onChange={this.handleInputChange}
-          
-        />
         <input type="submit" value="Post"/>
 
   
       </form>
+      </div>
+
+      <div className='Post-form' style={{width:'20px', display:''}}>
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="input"
+          name="gifTitle"
+          placeholder="title"
+          value={this.state.gifTitle}
+          onChange={this.handleInputChange}
+          required
+        />
+      <input
+          type="file"
+          name="image"
+          onChange={this.handleImageChange}
+          id="image"          
+        />
+        <input type="submit" value="upload"/>
+
+  
+      </form>
+      </div>
+      <div style={{color: 'green'}}> 
+      {this.state.status}
       </div>
       </div>
     );
